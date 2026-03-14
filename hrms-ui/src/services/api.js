019925e11872e43c6afb4bc166bb9b6b6c18e2a6
Suhail Nagore <mohdsuhail2232@gmail.com>
2026-03-14 22:35:26 +0530

    Connect frontend to backend

--- hrms-ui/src/services/api.js
+++ hrms-ui/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import NProgress from "nprogress";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://hrmanagic-backend.onrender.com/api"
 });
 
 API.interceptors.request.use((config) => {
